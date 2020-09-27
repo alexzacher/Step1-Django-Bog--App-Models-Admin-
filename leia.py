3274bd63d11a45b35ff1c85fac5f50f7683178b6
Alexandre Zacher <alex_zacher4000@hotmail.com>
2020-09-27 09:54:28 +0100

    Create leia.py

---
+++ leia.py
@@ -0,0 +1,4 @@
+# Criado para o meu desenvolvimento com esse fremawork, minha maneira de aumentar o desempenho dos estudos descrevendo
+# todos os passos que eu fiz ate chegar ao ponto desejado da criacao do blog
+
+# A leitura da pasta projeto_blog.txt naos esta em ordem apresentado mas tambem contem todos os passos menos detalhados.
